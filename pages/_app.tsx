fe4fb33648216c236fc7546d20a3b1326de4145c
chogyejin <whrpwls96@naver.com>
2022-12-01 03:07:04 +0900

    init: 프로젝트 초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../styles/globalStyles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
