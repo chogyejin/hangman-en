cc2c0a4b985765fa1aae627e355791b043766460
chogyejin <whrpwls96@naver.com>
2022-12-14 16:05:57 +0900

    style: 결과 화면 텍스트, 버튼 스타일 추가

--- src/components/Retry.tsx
+++ src/components/Retry.tsx
@@ -9,6 +9,7 @@ interface Props {
 
 const Retry = ({ result, resetGame, word }: Props) => {
   const { isLose, isWin } = result;
+
   const handleClick = () => {
     resetGame();
   };
@@ -17,14 +18,16 @@ const Retry = ({ result, resetGame, word }: Props) => {
     <Container>
       {isLose && (
         <>
-          <div>You lose</div>
+          <div>You lose!</div>
           <Button onClick={handleClick}>Retry?</Button>
         </>
       )}
       {isWin && (
         <>
-          <div>You win</div>
-          <div>Answer is {word}</div>
+          <div>You win!</div>
+          <AnswerText>
+            Answer is <span>{word}</span>
+          </AnswerText>
           <Button onClick={handleClick}>New game?</Button>
         </>
       )}
@@ -37,8 +40,51 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  font-size: 20px;
 `;
 
-const Button = styled.button``;
+const AnswerText = styled.div`
+  & span {
+    font-weight: 700;
+  }
+`;
+
+const Button = styled.button`
+  position: relative;
+  width: 200px;
+  height: 40px;
+  font-weight: 700;
+  border: 1px solid #000;
+  color: #fff;
+  background-color: #000;
+  transition: all 0.3s ease;
+  z-index: 1;
+  margin-top: 20px;
+
+  &:hover {
+    color: #000;
+  }
+
+  &:after {
+    position: absolute;
+    content: "";
+    width: 0;
+    height: 100%;
+    top: 0;
+    right: 0;
+    z-index: -1;
+    background: #fff;
+    transition: all 0.3s ease;
+  }
+
+  &:hover:after {
+    left: 0;
+    width: 100%;
+  }
+
+  &:active {
+    top: 3px;
+  }
+`;
 
 export default Retry;
