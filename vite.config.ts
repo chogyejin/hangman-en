e592e16be0c1ab888e69a2e58e49c2ab49be818b
chogyejin <whrpwls96@naver.com>
2023-12-24 20:49:00 +0900

    chore: add vitest package and vite config file

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
+});
