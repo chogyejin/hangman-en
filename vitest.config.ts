12dffb0b2d4699695fd4b83e984dde7b46730dde
chogyejin <whrpwls96@naver.com>
2024-02-12 19:22:33 +0900

    chore: add package for canvas mock in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
+    setupFiles: ["./vitest.setup.ts"],
     environment: "jsdom",
     globals: true,
+    deps: {
+      optimizer: {
+        web: {
+          include: ["vitest-canvas-mock"],
+        },
+      },
+    },
   },
 });
